f26d67a45106e425e288bf055ba026541f736118
m-shimizu <62657297+m-shimizu-ring@users.noreply.github.com>
2022-02-14 18:22:26 +0900

    テーブル変更

--- src/components/Probando.vue
+++ src/components/Probando.vue
@@ -1,59 +1,79 @@
 <template>
-  <div id="app">
-    <h1>Componente Probando</h1>
-    <v-select :options="provincias"></v-select>
-    <v-data-table
-      striped
-      :headers="headers"
-      :items="items"
-      :page.sync="page"
-      :items-per-page="5"
-      hide-default-footer
-      class="elevation-1"
-    >
-      <template v-slot:item.actions="{ item }">
-        <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
-        <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
-      </template>
-    </v-data-table>
-    <v-pagination v-model="page" :length="2"></v-pagination>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="10">
+        <v-data-table :headers="headers" :items="serverDatas">
+          <template v-slot:[`item.delete`]="{ item }">
+            <v-btn small color="error" @click="deleteItem(item)"> 削除 </v-btn>
+          </template>
+        </v-data-table>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
-      page: 1,
       headers: [
         {
-          text: "Nombre",
-          align: "start",
-          sortable: true,
-          value: "nombre",
+          text: "ID",
+          value: "id",
         },
-        { text: "Apellidos", value: "apellidos" },
-        { text: "Edad", value: "edad" },
-        { text: "Actions", value: "actions", sortable: false },
+        {
+          text: "名前",
+          value: "name",
+        },
+        { text: "メルアド", value: "email" },
+        {
+          text: "削除",
+          value: "delete",
+          sortable: false,
+        },
       ],
+      serverDatas: [],
+
       items: [
-        { nombre: "Juan Ramón", apellidos: "Perez", edad: 34 },
-        { nombre: "Manuel", apellidos: "Roma", edad: 24 },
-        { nombre: "Sara", apellidos: "Vázquez", edad: 54 },
-        { nombre: "Mónica", apellidos: "Tui", edad: 20 },
-        { nombre: "Raúl", apellidos: "Tui", edad: 20 },
-        { nombre: "Federico", apellidos: "Tui", edad: 20 },
-        { nombre: "Moncho", apellidos: "Tui", edad: 20 },
+        {
+          id: 1,
+          name: "高田健志",
+          age: 33,
+        },
+        {
+          id: 2,
+          name: "横山緑",
+          age: 42,
+        },
+        {
+          id: 3,
+          name: "山田太郎",
+          age: 10,
+        },
       ],
-      provincias: ["coruña", "Pontevedra"],
     };
   },
+
   methods: {
-    editItem(item) {
-      console.log("Editando");
-    },
     deleteItem(item) {
-      console.log("Borrando");
+      const index = this.serverDatas.indexOf(item);
+      confirm("ガチで削除しますか") && this.serverDatas.splice(index, 1);
     },
+  },
+  mounted() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        this.serverDatas = res.data;
+      })
+      .catch((e) => {
+        console.log(e);
+      })
+      .finally(() => {
+        console.log("通信完了");
+      });
   },
 };
 </script>
